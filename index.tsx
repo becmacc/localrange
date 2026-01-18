7e21f4c7db89559460d48f7b2d4ab042e71a0f39
Bechara Maccise <becharamacc@gmail.com>
2026-01-18 16:52:15 +0200

    Add console logging for debugging

--- index.tsx
+++ index.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('Starting app...');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error('Root element not found!');
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log('Root element found, rendering app...');
+
+try {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log('App rendered successfully!');
+} catch (error) {
+  console.error('Error rendering app:', error);
+  throw error;
+}
